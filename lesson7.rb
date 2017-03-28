f08b7e7537a4b235193a7f7c83bb4358c6f51770
Paulina Ika Sudjatmiko <Paulina@Paulina.local>
2017-03-29 08:54:47 +1100

    creating a simple menu

---
+++ lesson7.rb
@@ -0,0 +1,24 @@
+# initial_condition
+user_input = ""
+# while condition
+while user_input != 3
+
+  puts "1. Draw a box"
+  puts "2. Draw a triangle"
+  puts "3. Quit"
+
+  print "Enter your selection:"
+  user_input = gets.to_i
+
+  if user_input == 1
+    puts "Drawing a box"
+  elsif user_input == 2
+    puts "Drawing a triangle"
+  elsif user_input == 3
+    puts "Quiting"
+  else
+    puts "Error. You have entered the wrong input."
+    puts "Enter a number between 1-3."
+  end
+
+end
